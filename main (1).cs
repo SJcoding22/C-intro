5035ab7994b2a4ae36af4439c3c7afeb70177bbf
SJcoding22 <114853141+SJcoding22@users.noreply.github.com>
2023-01-28 19:38:30 +0900

    Add files via upload

---
+++ main (1).cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string w = "banana";
+        int x = 10;
+        string y = "green";
+        double z = 9.999999999;
+        if (x > z)
+        {
+            Console.WriteLine("Teacher is smart.");
+            Console.Write("HoHoHo");
+            Console.Write("HeyHeyHey");
+        }
+        else if (x = z)
+        {
+            Console.WriteLine("Teacher is so...")
+        }
+        else
+        {
+            Console.WriteLine("Teacher is so funny!");
+        }
+
+        int time = 20;
+
+        // string result = (time<18) ? "Good Day" : "Good Evening";
+        // Console.WriteLine(result);
+    }
+}
